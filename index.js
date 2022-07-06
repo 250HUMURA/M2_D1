d1d30f7fcad57ad81928cfa977a26985f461fcc0
kennedy <kennedymupenzih@outlook.com>
2022-07-06 16:49:26 +0200

    JS web dev day1

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// Spaghetti alla carbonara Recipe:
+
+let spaghetti = 400;
+let guanciale = 250;
+let egg = 6;
+let cheese = 50;
+let pepper = 4;
+
+// combination of blck pepper, egg yolks and romano cheese
+let roast = pepper + egg + cheese;
+console.log(roast);
+
+// combination of guanciale and romano
+let pan = guanciale + roast;
+console.log(pan);
+ 
